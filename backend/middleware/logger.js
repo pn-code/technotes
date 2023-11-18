75d51c98adc2f4355ebc1cbd697fa86d08687689
Philip Nguyen <philipnguyen953@gmail.com>
2023-11-17 22:13:33 -0800

    Created logger middleware

---
+++ backend/middleware/logger.js
@@ -0,0 +1,31 @@
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+const path = require("path");
+
+const logEvents = async (message, logFileName) => {
+    const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+
+    try {
+        const logsDirectory = path.join(__dirname, "..", "logs");
+        const logDirectory = path.join(logsDirectory, logFileName)
+
+        if (!fs.existsSync(logsDirectory)) {
+            await fsPromises.mkdir(logsDirectory);
+        }
+
+        await fsPromises.appendFile(logDirectory, logItem);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const logger = (req, res, next) => {
+    logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, "reqLog.log");
+    console.log(`${req.method} ${req.path}`);
+    next();
+};
+
+module.exports = { logger, logEvents };
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
 const express = require("express");
-
-const PORT = process.env.PORT | 3500;
 const path = require("path");
+const PORT = process.env.PORT | 3500
 
+const { logger } = require("./middleware/logger");
+
+// Initiate our express server
 const app = express();
+
+// Allow server to write logs
+app.use(logger);
+
+// Allow server to parse json in the body
+app.use(express.json());
 
 // Allow server to look into public folder for assets
 app.use("/", express.static(path.join(__dirname, "/public")));
@@ -13,17 +21,17 @@ app.use("/", require("./routes/root"));
 
 // Serve 404 page if no route was hit
 app.all("*", (req, res) => {
-  const notFoundHtmlPage = path.join(__dirname, "views", "404.html");
+    const notFoundHtmlPage = path.join(__dirname, "views", "404.html");
 
-  res.status(404);
+    res.status(404);
 
-  if (req.accepts("html")) {
-    res.sendFile(notFoundHtmlPage);
-  } else if (req.accepts("json")) {
-    res.json({ message: "404 Not Found" });
-  } else {
-    res.type("txt").send("404 Not Found");
-  }
+    if (req.accepts("html")) {
+        res.sendFile(notFoundHtmlPage);
+    } else if (req.accepts("json")) {
+        res.json({ message: "404 Not Found" });
+    } else {
+        res.type("txt").send("404 Not Found");
+    }
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
