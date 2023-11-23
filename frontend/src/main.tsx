5c528fe6f8294813e8c3697d69a1d89358d32de4
Philip Nguyen <philipnguyen953@gmail.com>
2023-11-23 15:01:28 -0800

    Set up react-redux store and apiSlice

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,8 +4,13 @@ import { RouterProvider } from "react-router-dom";
 import router from "../lib/router";
 import "./index.css";
 
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <RouterProvider router={router} />
+        </Provider>
+    </React.StrictMode>
 );
