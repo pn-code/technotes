a78ba14148b3885e41d1237b02583e04742fee47
Philip Nguyen <philipnguyen953@gmail.com>
2023-11-26 09:44:19 -0800

    Added disabled prop in Button

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -1,31 +1,34 @@
 import React from "react";
 
 interface ButtonProps {
-  onClick?: () => void;
-  children: React.ReactNode;
-  className?: string;
-  ariaLabel?: string;
-  title?: string;
+    onClick?: () => void;
+    children: React.ReactNode;
+    className?: string;
+    ariaLabel?: string;
+    title?: string;
+    disabled?: boolean;
 }
 
 export default function Button({
-  onClick,
-  className,
-  children,
-  ariaLabel,
-  title,
+    onClick,
+    className,
+    children,
+    ariaLabel,
+    title,
+    disabled,
 }: ButtonProps) {
-  return (
-    <button
-      title={title}
-      aria-label={ariaLabel}
-      className={
-        className ||
-        "py-1.5 px-4 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 ease-linear duration-100"
-      }
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  );
+    return (
+        <button
+            title={title}
+            aria-label={ariaLabel}
+            className={
+                className ||
+                "py-1.5 px-4 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 ease-linear duration-100"
+            }
+            onClick={onClick}
+            disabled={disabled}
+        >
+            {children}
+        </button>
+    );
 }
