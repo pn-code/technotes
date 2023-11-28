b26d695191ad9f0f4cac882b75872025244cfa8e
Philip Nguyen <philipnguyen953@gmail.com>
2023-11-27 20:31:39 -0800

    Removed store from imports

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,4 +1,4 @@
-import { Store, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/apiSlice";
 import { setupListeners } from "@reduxjs/toolkit/query";
 
