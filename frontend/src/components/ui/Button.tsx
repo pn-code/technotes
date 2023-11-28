8052fc3ed8264aa6b140c7ab6461f4fc7573e03c
Philip Nguyen <philipnguyen953@gmail.com>
2023-11-27 19:32:56 -0800

    Added type prop to Button

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -1,34 +1,37 @@
 import React from "react";
 
 interface ButtonProps {
-    onClick?: () => void;
-    children: React.ReactNode;
-    className?: string;
-    ariaLabel?: string;
-    title?: string;
-    disabled?: boolean;
+  onClick?: () => void;
+  children: React.ReactNode;
+  className?: string;
+  ariaLabel?: string;
+  title?: string;
+  disabled?: boolean;
+  type?: "submit" | "button" | "reset";
 }
 
 export default function Button({
-    onClick,
-    className,
-    children,
-    ariaLabel,
-    title,
-    disabled,
+  onClick,
+  className,
+  children,
+  ariaLabel,
+  title,
+  disabled,
+  type,
 }: ButtonProps) {
-    return (
-        <button
-            title={title}
-            aria-label={ariaLabel}
-            className={
-                className ||
-                "py-1.5 px-4 disabled:bg-indigo-600/30 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 ease-linear duration-100"
-            }
-            onClick={onClick}
-            disabled={disabled}
-        >
-            {children}
-        </button>
-    );
+  return (
+    <button
+      title={title}
+      aria-label={ariaLabel}
+      className={
+        className ||
+        "py-1.5 px-4 disabled:bg-indigo-600/30 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 ease-linear duration-100"
+      }
+      onClick={onClick}
+      disabled={disabled}
+      type={type ? type : "button"}
+    >
+      {children}
+    </button>
+  );
 }
