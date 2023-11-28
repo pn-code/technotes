2cc1612b7ba8e6589738537def7b8c3d5b0088ea
Philip Nguyen <philipnguyen953@gmail.com>
2023-11-28 13:09:54 -0800

    Created login rate limiter middleware

---
+++ backend/middleware/loginLimiter.js
@@ -0,0 +1,21 @@
+const rateLimit = require("express-rate-limit");
+const { logEvents } = require("./logger");
+
+const loginLimiter = rateLimit({
+  windowMs: 60 * 1000, // 1 min
+  max: 5, // 5 requests per min per IP
+  message: {
+    message: "Too many login attempts. Please try again later.",
+  },
+  handler: (req, res, next, options) => {
+    logEvents(
+      `Too Many Requests: ${options.message.message}\t${req.method}\t${req.url}\t${req.headers.origin}`,
+      "errLog.log"
+    );
+    res.status(options.statusCode).send(options.message);
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+module.exports = loginLimiter;
