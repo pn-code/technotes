86b206bc1f15720e0e2f2f14f60d505016265271
Philip Nguyen <philipnguyen953@gmail.com>
2023-11-29 14:49:38 -0800

    Added logout function to DashHeader

--- frontend/src/components/dashboard/DashHeader.tsx
+++ frontend/src/components/dashboard/DashHeader.tsx
@@ -1,7 +1,28 @@
-import { Link } from "react-router-dom";
-import { Home, StickyNote, User } from "lucide-react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Fingerprint, Home, StickyNote, User } from "lucide-react";
+import { useLogOutMutation } from "../../features/auth/authApiSlice";
+import Button from "../ui/Button";
+import { useEffect } from "react";
 
 export default function DashHeader() {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const [logOut, { isLoading, isSuccess, isError, error }] =
+    useLogOutMutation();
+
+  useEffect(() => {
+    if (isSuccess) navigate("/");
+  }, [isSuccess, navigate]);
+
+  const handleLogOut = () => logOut(null);
+
+  if (isLoading) return <p>Logging Out...</p>;
+
+  const errorObj = error as any;
+
+  if (isError) return <p>Error: {errorObj?.data?.message}</p>;
+
   return (
     <header className="px-2">
       <div className="py-1 flex gap-2 md:gap-8 items-center border-y-2 border-slate-400">
@@ -30,6 +51,15 @@ export default function DashHeader() {
               >
                 <User size={18} /> Users
               </Link>
+            </li>
+            <li>
+              <Button
+                onClick={handleLogOut}
+                type="button"
+                className="flex gap-1 items-center font-semibold hover:text-slate-600"
+              >
+                <Fingerprint size={18} /> Log Out
+              </Button>
             </li>
           </ul>
         </nav>
