265035e5f57dc102a8cd1116157def1b1874f5e2
Philip Nguyen <philipnguyen953@gmail.com>
2023-12-01 11:34:06 -0800

    Added isLoggedIn props to check for user

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -5,6 +5,7 @@ import { jwtDecode } from "jwt-decode";
 export default function useAuth() {
   const token = useSelector(selectCurrentToken);
 
+  let isLoggedIn = false;
   let isManager = false;
   let isAdmin = false;
   let status = "Employee";
@@ -13,14 +14,15 @@ export default function useAuth() {
     const decoded = jwtDecode(token) as any;
     const { username, roles } = decoded.UserInfo;
 
+    isLoggedIn = true;
     isManager = roles.includes("Manager");
     isAdmin = roles.includes("Admin");
 
     if (isManager) status = "Manager";
     if (isAdmin) status = "Admin";
 
-    return { username, roles, status, isManager, isAdmin };
+    return { username, roles, status, isManager, isAdmin, isLoggedIn };
   }
 
-  return { username: "", roles: [], isManager, isAdmin, status };
+  return { username: "", roles: [], isManager, isAdmin, status, isLoggedIn };
 }
